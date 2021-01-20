ab269665a8691bbf8e5ffa4e7b59df6cf90316ba
Oleh Vanin <exsesx@gmail.com>
2021-01-20 15:06:09 +0200

    feat: add basic routing

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,12 +1,23 @@
 import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import About from './About';
+import Header from './Header';
 import Home from './Home';
 
 interface AppProps {}
 
 function App({}: AppProps) {
   return (
-    <div className="bg-white dark:bg-gray-800 h-screen">
-      <Home />
+    <div className="bg-white dark:bg-gray-800 h-screen flex flex-col">
+      <Header />
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
     </div>
   );
 }
