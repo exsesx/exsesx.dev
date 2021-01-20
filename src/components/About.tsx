054f9ff7ff2e0dccc6a84eb414b435b5300213e6
Oleg Vanin <ovanin@s-pro.io>
2021-01-20 15:06:09 +0200

    Add basic routing

---
+++ src/components/About.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+export default function About() {
+  return (
+    <div className="container mx-auto flex h-full justify-center items-center">
+      <h1 className="font-bold dark:text-white text-4xl">In Progress...</h1>
+    </div>
+  );
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,15 @@
 const colors = require('tailwindcss/colors');
 
 module.exports = {
-  purge: ['./src/**/*.{js,ts,jsx,tsx}', "./public/**/*.html"],
+  purge: ['./src/**/*.{js,ts,jsx,tsx}', './public/**/*.html'],
   darkMode: 'media',
   theme: {
     colors: {
+      transparent: 'transparent',
       gray: colors.trueGray,
       blue: colors.lightBlue,
+      white: colors.white,
+      teal: colors.teal,
     },
     extend: {},
   },
