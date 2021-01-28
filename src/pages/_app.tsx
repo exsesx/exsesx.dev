08ba4066d7496bff3fa2380606b9112731fe9466
Oleh Vanin <exsesx@gmail.com>
2021-01-28 22:10:48 +0200

    style: update Tailwind styles

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import "tailwindcss/tailwind.css";
 import Layout from "../components/Layout";
 import "../styles/globals.css";
 
