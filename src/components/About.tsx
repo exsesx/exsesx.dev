f1717eadfb12cad1f765df8ba0372254b223f17f
Oleg Vanin <ovanin@s-pro.io>
2021-01-28 18:16:08 +0200

    Starting over

--- src/components/About.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function About() {
-  return (
-    <main className="container mx-auto flex h-full justify-center items-center">
-      <h1 className="font-bold dark:text-white text-4xl">In Progress...</h1>
-    </main>
-  );
-}
