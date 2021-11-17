61e093b1d9e5815d8905712adc5f107c841826f9
Oleg Vanin <ovanin@s-pro.io>
2021-11-17 21:09:17 +0200

    feat: replace icons; remove unused dependencies; add tailwind typography plugin

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 const colors = require("tailwindcss/colors");
 
 module.exports = {
-  mode: 'jit',
+  mode: "jit",
   purge: ["./src/**/*.{js,jsx,ts,tsx}"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
@@ -20,5 +20,5 @@ module.exports = {
       display: ["dark"],
     },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/typography")],
 };
