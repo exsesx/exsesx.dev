65de89d8778a6beca74c48bc4be4e77577315cfb
Oleh Vanin <exsesx@gmail.com>
2021-11-17 17:01:10 +0200

    fix: projects - reduce scale animation a bit

---
+++ src/components/Dock/index.tsx
@@ -0,0 +1,14 @@
+import classNames from "classnames";
+import styles from "./Dock.module.scss";
+
+export default function Dock() {
+  const containerClasses = classNames("fixed bottom-1 flex px-4 left-1/2 transform -translate-x-1/2", styles.container);
+
+  return (
+    <footer className={containerClasses}>
+      <button>
+        <img src="https://rauno.me/static/bg.png" alt="" />
+      </button>
+    </footer>
+  );
+}
