a570040b32216b6ca816408021fc2cac5449ef88
Oleh Vanin <exsesx@gmail.com>
2022-01-29 00:59:19 +0200

    feat: add animated theme switcher

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,14 +1,37 @@
-import Image from "next/image";
-import { useEffect, useState } from "react";
+import { SVGProps, useEffect } from "react";
+import { animated, useSpring } from "react-spring";
+import useLocalStorage from "../lib/hooks/useLocalStorage";
 
 type Mode = "light" | "dark";
 
-// TODO: Persist from localStorage
+const ANIMATION_PROPERTIES: Record<Mode, SVGProps<SVGCircleElement>> = {
+  dark: {
+    r: 9,
+    transform: "rotate(40deg)",
+    cx: 12,
+    cy: 4,
+    opacity: 0,
+  },
+  light: {
+    r: 5,
+    transform: "rotate(90deg)",
+    cx: 30,
+    cy: 0,
+    opacity: 1,
+  },
+};
+const ANIMATION_SPRING_CONFIG = { mass: 4, tension: 250, friction: 35 };
+const ICON_SIZE = 24;
+
 export default function ThemeSwitcher() {
   const defaultMode: Mode =
     typeof window !== "undefined" && window.matchMedia("(prefers-color-scheme: dark)").matches ? "dark" : "light";
 
-  const [mode, setMode] = useState<Mode>(defaultMode);
+  const [mode, setMode] = useLocalStorage<Mode>("exsesx:color-scheme", defaultMode);
+
+  const toggleDarkMode = () => {
+    return setMode(mode === "light" ? "dark" : "light");
+  };
 
   useEffect(() => {
     if (mode === "dark") {
@@ -18,9 +41,62 @@ export default function ThemeSwitcher() {
     }
   }, [mode]);
 
+  const { r, transform, cx, cy, opacity } = ANIMATION_PROPERTIES[mode === "dark" ? "dark" : "light"];
+
+  const svgContainerProps = useSpring({
+    to: {
+      transform,
+    },
+    config: ANIMATION_SPRING_CONFIG,
+  });
+  const centerCircleProps = useSpring({ r, config: ANIMATION_SPRING_CONFIG });
+  const maskedCircleProps = useSpring({
+    cx,
+    cy,
+    config: ANIMATION_SPRING_CONFIG,
+  });
+  const linesProps = useSpring({ opacity, config: ANIMATION_SPRING_CONFIG });
+
   return (
-    <button className="flex" onClick={() => setMode(prevState => (prevState === "light" ? "dark" : "light"))}>
-      <Image src={mode === "dark" ? "/icons/light_mode.svg" : "/icons/dark_mode.svg"} width={24} height={24} />
-    </button>
+    <animated.svg
+      xmlns="http://www.w3.org/2000/svg"
+      width={ICON_SIZE}
+      height={ICON_SIZE}
+      viewBox="0 0 24 24"
+      fill="none"
+      className="text-gray-800 dark:text-white"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      stroke="currentColor"
+      onClick={toggleDarkMode}
+      style={{
+        cursor: "pointer",
+        ...svgContainerProps,
+      }}
+    >
+      <mask id="myMask2">
+        <rect x="0" y="0" width="100%" height="100%" className="fill-white" />
+        <animated.circle {...maskedCircleProps} r="9" fill="#000" />
+      </mask>
+
+      <animated.circle
+        className="fill-gray-800 dark:fill-white"
+        cx="12"
+        cy="12"
+        {...centerCircleProps}
+        mask="url(#myMask2)"
+      />
+      <animated.g stroke="currentColor" style={linesProps}>
+        <line x1="12" y1="1" x2="12" y2="3" />
+        <line x1="12" y1="21" x2="12" y2="23" />
+        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+        <line x1="1" y1="12" x2="3" y2="12" />
+        <line x1="21" y1="12" x2="23" y2="12" />
+        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+      </animated.g>
+    </animated.svg>
   );
 }
