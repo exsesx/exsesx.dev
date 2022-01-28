1e7fd838a9f9966c5c85f9bd14b1d2a286dfa363
Oleh Vanin <exsesx@gmail.com>
2022-01-29 01:46:55 +0200

    fix: theme-color and other improvements

--- src/components/ThemeSwitcher.tsx
+++ src/components/ThemeSwitcher.tsx
@@ -1,3 +1,4 @@
+import Head from "next/head";
 import { SVGProps, useEffect } from "react";
 import { animated, useSpring } from "react-spring";
 import useLocalStorage from "../lib/hooks/useLocalStorage";
@@ -58,45 +59,51 @@ export default function ThemeSwitcher() {
   const linesProps = useSpring({ opacity, config: ANIMATION_SPRING_CONFIG });
 
   return (
-    <animated.svg
-      xmlns="http://www.w3.org/2000/svg"
-      width={ICON_SIZE}
-      height={ICON_SIZE}
-      viewBox="0 0 24 24"
-      fill="none"
-      className="text-gray-800 dark:text-white"
-      strokeWidth="2"
-      strokeLinecap="round"
-      strokeLinejoin="round"
-      stroke="currentColor"
-      onClick={toggleDarkMode}
-      style={{
-        cursor: "pointer",
-        ...svgContainerProps,
-      }}
-    >
-      <mask id="myMask2">
-        <rect x="0" y="0" width="100%" height="100%" className="fill-white" />
-        <animated.circle {...maskedCircleProps} r="9" fill="#000" />
-      </mask>
+    <>
+      <Head>
+        <meta name="msapplication-TileColor" content={mode === "dark" ? "#262626" : "#ffffff"} />
+        <meta name="theme-color" content={mode === "dark" ? "#262626" : "#ffffff"} />
+      </Head>
+      <animated.svg
+        xmlns="http://www.w3.org/2000/svg"
+        width={ICON_SIZE}
+        height={ICON_SIZE}
+        viewBox="0 0 24 24"
+        fill="none"
+        className="text-gray-800 dark:text-white"
+        strokeWidth="2"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        stroke="currentColor"
+        onClick={toggleDarkMode}
+        style={{
+          cursor: "pointer",
+          ...svgContainerProps,
+        }}
+      >
+        <mask id="myMask2">
+          <rect x="0" y="0" width="100%" height="100%" className="fill-white" />
+          <animated.circle {...maskedCircleProps} r="9" fill="#000" />
+        </mask>
 
-      <animated.circle
-        className="fill-gray-800 dark:fill-white"
-        cx="12"
-        cy="12"
-        {...centerCircleProps}
-        mask="url(#myMask2)"
-      />
-      <animated.g stroke="currentColor" style={linesProps}>
-        <line x1="12" y1="1" x2="12" y2="3" />
-        <line x1="12" y1="21" x2="12" y2="23" />
-        <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
-        <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
-        <line x1="1" y1="12" x2="3" y2="12" />
-        <line x1="21" y1="12" x2="23" y2="12" />
-        <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
-        <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
-      </animated.g>
-    </animated.svg>
+        <animated.circle
+          className="fill-gray-800 dark:fill-white"
+          cx="12"
+          cy="12"
+          {...centerCircleProps}
+          mask="url(#myMask2)"
+        />
+        <animated.g stroke="currentColor" style={linesProps}>
+          <line x1="12" y1="1" x2="12" y2="3" />
+          <line x1="12" y1="21" x2="12" y2="23" />
+          <line x1="4.22" y1="4.22" x2="5.64" y2="5.64" />
+          <line x1="18.36" y1="18.36" x2="19.78" y2="19.78" />
+          <line x1="1" y1="12" x2="3" y2="12" />
+          <line x1="21" y1="12" x2="23" y2="12" />
+          <line x1="4.22" y1="19.78" x2="5.64" y2="18.36" />
+          <line x1="18.36" y1="5.64" x2="19.78" y2="4.22" />
+        </animated.g>
+      </animated.svg>
+    </>
   );
 }
