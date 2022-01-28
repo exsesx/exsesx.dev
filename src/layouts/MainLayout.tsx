0b216c4679d4341a44eba2be19fa1843a1e3fd72
Oleg Vanin <ovanin@s-pro.io>
2022-01-29 00:59:19 +0200

    feat: add animated theme switcher

--- src/layouts/MainLayout.tsx
+++ src/layouts/MainLayout.tsx
@@ -8,7 +8,7 @@ interface LayoutProps {
 
 export default function MainLayout({ children }: LayoutProps) {
   return (
-    <div className="bg-white dark:bg-gray-800 w-screen h-screen flex flex-col overflow-auto">
+    <div className="bg-white dark:bg-gray-800 transition-colors duration-200 w-screen h-screen flex flex-col overflow-auto">
       <Header />
       {children}
       <VersionTag />
