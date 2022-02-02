387c22da59abfa134db3cd7cd84930eced8466e0
Oleh Vanin <exsesx@gmail.com>
2022-02-03 00:03:15 +0200

    fix: restore the synchronous no-flash script
    - defer the Script approach until Next.js supports beforeInteractive

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,5 @@
 import { AppProps } from "next/app";
 import Head from "next/head";
-import Script from "next/script";
 import MainLayout from "../layouts/MainLayout";
 import "../styles/globals.css";
 
@@ -11,12 +10,9 @@ function MyApp({ Component, pageProps }: AppProps) {
         <title>Oleh Vanin - Personal Web Page</title>
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <>
-        <Script src="scripts/noflash.js" strategy="beforeInteractive" />
-        <MainLayout>
-          <Component {...pageProps} />
-        </MainLayout>
-      </>
+      <MainLayout>
+        <Component {...pageProps} />
+      </MainLayout>
     </>
   );
 }
