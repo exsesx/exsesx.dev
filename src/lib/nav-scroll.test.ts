e9e28068647598d03b010e12e2b28a9ea0ba7147
Oleh Vanin <exsesx@gmail.com>
2026-06-04 20:38:53 +0200

    fix: make repeated nav clicks scroll to top (#4)

---
+++ src/lib/nav-scroll.test.ts
@@ -0,0 +1,20 @@
+import { describe, expect, test } from "bun:test";
+import { shouldScrollToTopForNavClick } from "./nav-scroll";
+
+describe("shouldScrollToTopForNavClick", () => {
+  test("scrolls to top when clicking the current home route below the top", () => {
+    expect(shouldScrollToTopForNavClick({ pathname: "/", href: "/", scrollY: 24 })).toBe(true);
+  });
+
+  test("scrolls to top when clicking the current projects route below the top", () => {
+    expect(shouldScrollToTopForNavClick({ pathname: "/projects", href: "/projects", scrollY: 24 })).toBe(true);
+  });
+
+  test("does not intercept clicks at the top of the current route", () => {
+    expect(shouldScrollToTopForNavClick({ pathname: "/", href: "/", scrollY: 0 })).toBe(false);
+  });
+
+  test("does not intercept navigation to a different route", () => {
+    expect(shouldScrollToTopForNavClick({ pathname: "/project/flowkit", href: "/projects", scrollY: 24 })).toBe(false);
+  });
+});
